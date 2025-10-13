03df4d5b534923665f00de13526bb90549922118
Isaac <nisaacdz@gmail.com>
2025-10-13 04:37:14 +0000

    fix eventsHandler

--- features/api/v1/events/eventsHandler.ts
+++ features/api/v1/events/eventsHandler.ts
@@ -1,16 +1,19 @@
-import { ApiResponse } from "@/shared/types";
-import { CreateEventSchema } from "@/shared/validators";
-import { Event, Events } from "@/db/schema/activities";
+import { ApiResponse, AppError } from "@/shared/types";
+import { CreateEventSchema } from "@/shared/validators/activities";
+import { Events } from "@/db/schema/activities";
 import z from "zod";
-import db from "@/configs/db";
+import pgPool from "@/configs/db";
 
 export async function createEvent(
-  newEvent: z.infer<typeof CreateEventSchema>,
-): Promise<ApiResponse<Event>> {
-  const [event] = await db.insert(Events).values(newEvent).returning();
+  newEvent: z.infer<typeof CreateEventSchema>
+): Promise<ApiResponse<null>> {
+  const d = await pgPool.db.insert(Events).values(newEvent).returning();
+  if (!d?.length) {
+    throw new AppError("A server error occurred", 500);
+  }
   return {
     success: true,
     message: "Event created successfully",
-    data: event,
+    data: null,
   };
 }
