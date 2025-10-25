9acc0383bc68de775b9a95ea0ea12b16459d7717
Isaac <nisaacdz@gmail.com>
2025-10-25 01:16:52 +0000

    fix wrong migrations

--- db/migrations/0002_first_white_queen.sql
+++ db/migrations/0002_first_white_queen.sql
@@ -1,4 +1,5 @@
-ALTER TABLE "core"."chapters" RENAME COLUMN "is_active" TO "archived_at";--> statement-breakpoint
+ALTER TABLE "core"."chapters" ADD COLUMN "archived_at" date;--> statement-breakpoint
 ALTER TABLE "core"."committees" ADD COLUMN "archived_at" date;--> statement-breakpoint
 ALTER TABLE "core"."chapter_memberships" DROP COLUMN "is_active";--> statement-breakpoint
+ALTER TABLE "core"."chapters" DROP COLUMN "is_active";--> statement-breakpoint
 ALTER TABLE "core"."committee_memberships" DROP COLUMN "is_active";
