ac3af4d5b405996c8cc57d4f41db0eb3ad6bb71a
Isaac <nisaacdz@gmail.com>
2025-11-02 01:11:00 +0000

    Update vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from "vitest/config";
 import path from "path";
-import dotenv from "dotenv";
-
-dotenv.config({ path: ".env.test" });
 
 export default defineConfig({
   test: {
