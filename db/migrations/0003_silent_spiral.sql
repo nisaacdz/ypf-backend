df2d0f2bd6d93581927f1e6fc1ae7b8c569108b2
Isaac <nisaacdz@gmail.com>
2025-11-04 12:46:53 +0000

    reset db migrations

--- db/migrations/0003_silent_spiral.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "shop"."product_photos" ADD COLUMN "medium_id" uuid NOT NULL;--> statement-breakpoint
-ALTER TABLE "shop"."product_photos" ADD CONSTRAINT "product_photos_medium_id_media_id_fk" FOREIGN KEY ("medium_id") REFERENCES "core"."media"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "shop"."product_photos" DROP COLUMN "photo_url";
