2c8ce09e68faeb32d5cec018262d29787ca0bc75
Isaac <nisaacdz@gmail.com>
2025-11-17 00:35:21 +0000

    Add validator for chapter update

--- shared/validators/core.ts
+++ shared/validators/core.ts
@@ -15,3 +15,13 @@ export const GetCommitteesQuerySchema = z.object({
   chapterId: z.uuid({ message: "Invalid chapter ID format." }).optional(),
   ...PaginationQuery.shape,
 });
+
+export const UpdateChapterSchema = z
+  .object({
+    name: z.string().min(1, "Name is required").optional(),
+    description: z.string("Invalid description format.").optional(),
+    foundingDate: z.coerce.date("Invalid date format.").optional(),
+  })
+  .refine((data) => Object.keys(data).length > 0, {
+    message: "At least one field must be provided for update.",
+  });
