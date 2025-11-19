1a940bbc455d28fb5e268ac0f9bd62cdc2cde1fd
Isaac <nisaacdz@gmail.com>
2025-11-19 17:51:01 +0000

    Extract extended types from Express to IncomingMessage from http

---
+++ shared/types/http.d.ts
@@ -0,0 +1,12 @@
+import 'http';
+import { AuthenticatedUser } from '.';
+
+declare module 'http' {
+    interface IncomingMessage {
+      User?: AuthenticatedUser;
+      Body: any;
+      Query: any;
+      Params: any;
+      File: any;
+    }
+}
