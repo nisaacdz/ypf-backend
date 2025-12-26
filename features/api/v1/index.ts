8330585db599b805cd19d058d811e1aa744c15ec
Isaac <nisaacdz@gmail.com>
2025-12-26 20:16:59 +0000

    Evict welfare feature

--- features/api/v1/index.ts
+++ features/api/v1/index.ts
@@ -14,7 +14,7 @@ import announcementsRouter from "./announcements";
 import applicationsRouter from "./applications";
 import duesRouter from "./dues";
 import partnershipsRouter from "./partnerships";
-import welfareRouter from "./welfare";
+// import welfareRouter from "./welfare";
 import dashboardRouter from "./dashboard";
 import constituentsRouter from "./constituents";
 
@@ -35,7 +35,7 @@ apiRouter.use("/announcements", announcementsRouter);
 apiRouter.use("/applications", applicationsRouter);
 apiRouter.use("/dues", duesRouter);
 apiRouter.use("/partnerships", partnershipsRouter);
-apiRouter.use("/welfare", welfareRouter);
+//apiRouter.use("/welfare", welfareRouter);
 apiRouter.use("/dashboard", dashboardRouter);
 apiRouter.use("/constituents", constituentsRouter);
 
--- features/api/v1/welfare/welfareHandler.ts
+++ features/api/v1/welfare/welfareHandler.ts
@@ -1,142 +1,142 @@
-import { ApiError, ApiResponse } from "@/shared/types";
-import {
-  GetWelfareCasesQuerySchema,
-  CreateWelfareCaseSchema,
-  UpdateWelfareCaseSchema,
-} from "./schemas";
-import z from "zod";
-import { Paginated } from "@/shared/dtos";
-import { YPFWelfareCase, YPFWelfareCaseDetail } from "./dtos";
-import * as welfareService from "@/shared/services/welfareService";
-import dbClient from "@/configs/db";
-import schema from "@/db/schema";
-import { eq } from "drizzle-orm";
+// import { ApiError, ApiResponse } from "@/shared/types";
+// import {
+//   GetWelfareCasesQuerySchema,
+//   CreateWelfareCaseSchema,
+//   UpdateWelfareCaseSchema,
+// } from "./schemas";
+// import z from "zod";
+// import { Paginated } from "@/shared/dtos";
+// import { YPFWelfareCase, YPFWelfareCaseDetail } from "./dtos";
+// import * as welfareService from "@/shared/services/welfareService";
+// import dbClient from "@/configs/db";
+// import schema from "@/db/schema";
+// import { eq } from "drizzle-orm";
 
-export async function getWelfareCases(
-  query: z.infer<typeof GetWelfareCasesQuerySchema>,
-): Promise<ApiResponse<Paginated<YPFWelfareCase>>> {
-  const data = await welfareService.fetchWelfareCases(query);
+// export async function getWelfareCases(
+//   query: z.infer<typeof GetWelfareCasesQuerySchema>,
+// ): Promise<ApiResponse<Paginated<YPFWelfareCase>>> {
+//   const data = await welfareService.fetchWelfareCases(query);
 
-  return {
-    success: true,
-    message: "Welfare cases fetched successfully",
-    data,
-  };
-}
+//   return {
+//     success: true,
+//     message: "Welfare cases fetched successfully",
+//     data,
+//   };
+// }
 
-export async function getWelfareCase(
-  welfareCaseId: string,
-): Promise<ApiResponse<YPFWelfareCaseDetail>> {
-  const data = await welfareService.fetchWelfareCaseById(welfareCaseId);
+// export async function getWelfareCase(
+//   welfareCaseId: string,
+// ): Promise<ApiResponse<YPFWelfareCaseDetail>> {
+//   const data = await welfareService.fetchWelfareCaseById(welfareCaseId);
 
-  return {
-    success: true,
-    message: "Welfare case fetched successfully",
-    data,
-  };
-}
+//   return {
+//     success: true,
+//     message: "Welfare case fetched successfully",
+//     data,
+//   };
+// }
 
-export async function createWelfareCase(
-  newCase: z.infer<typeof CreateWelfareCaseSchema>,
-): Promise<ApiResponse<string>> {
-  const welfareCaseId = await welfareService.createWelfareCase(newCase);
+// export async function createWelfareCase(
+//   newCase: z.infer<typeof CreateWelfareCaseSchema>,
+// ): Promise<ApiResponse<string>> {
+//   const welfareCaseId = await welfareService.createWelfareCase(newCase);
 
-  return {
-    success: true,
-    message: "Welfare case created successfully",
-    data: welfareCaseId,
-  };
-}
+//   return {
+//     success: true,
+//     message: "Welfare case created successfully",
+//     data: welfareCaseId,
+//   };
+// }
 
-export async function updateWelfareCase(
-  welfareCaseId: string,
-  updates: z.infer<typeof UpdateWelfareCaseSchema>,
-): Promise<ApiResponse<null>> {
-  await welfareService.updateWelfareCase(welfareCaseId, updates);
+// export async function updateWelfareCase(
+//   welfareCaseId: string,
+//   updates: z.infer<typeof UpdateWelfareCaseSchema>,
+// ): Promise<ApiResponse<null>> {
+//   await welfareService.updateWelfareCase(welfareCaseId, updates);
 
-  return {
-    success: true,
-    message: "Welfare case updated successfully",
-    data: null,
-  };
-}
+//   return {
+//     success: true,
+//     message: "Welfare case updated successfully",
+//     data: null,
+//   };
+// }
 
-export async function deleteWelfareCase(
-  welfareCaseId: string,
-): Promise<ApiResponse<null>> {
-  await welfareService.deleteWelfareCase(welfareCaseId);
+// export async function deleteWelfareCase(
+//   welfareCaseId: string,
+// ): Promise<ApiResponse<null>> {
+//   await welfareService.deleteWelfareCase(welfareCaseId);
 
-  return {
-    success: true,
-    message: "Welfare case deleted successfully",
-    data: null,
-  };
-}
+//   return {
+//     success: true,
+//     message: "Welfare case deleted successfully",
+//     data: null,
+//   };
+// }
 
-export async function addWelfareCaseBeneficiaries(
-  welfareCaseId: string,
-  beneficiaryIds: string[],
-): Promise<ApiResponse<null>> {
-  const welfareCase = await dbClient.db.query.WelfareCases.findFirst({
-    where: eq(schema.WelfareCases.id, welfareCaseId),
-    columns: { id: true },
-  });
+// export async function addWelfareCaseBeneficiaries(
+//   welfareCaseId: string,
+//   beneficiaryIds: string[],
+// ): Promise<ApiResponse<null>> {
+//   const welfareCase = await dbClient.db.query.WelfareCases.findFirst({
+//     where: eq(schema.WelfareCases.id, welfareCaseId),
+//     columns: { id: true },
+//   });
 
-  if (!welfareCase) {
-    throw new ApiError("Welfare case not found", 404);
-  }
+//   if (!welfareCase) {
+//     throw new ApiError("Welfare case not found", 404);
+//   }
 
-  await welfareService.addWelfareCaseBeneficiaries(
-    welfareCaseId,
-    beneficiaryIds,
-  );
+//   await welfareService.addWelfareCaseBeneficiaries(
+//     welfareCaseId,
+//     beneficiaryIds,
+//   );
 
-  return {
-    success: true,
-    message: "YPFWelfareCase beneficiaries added successfully",
-    data: null,
-  };
-}
+//   return {
+//     success: true,
+//     message: "YPFWelfareCase beneficiaries added successfully",
+//     data: null,
+//   };
+// }
 
-export async function removeWelfareCaseBeneficiary(
-  welfareCaseId: string,
-  beneficiaryId: string,
-): Promise<ApiResponse<null>> {
-  const welfareCase = await dbClient.db.query.WelfareCases.findFirst({
-    where: eq(schema.WelfareCases.id, welfareCaseId),
-    columns: { id: true },
-  });
+// export async function removeWelfareCaseBeneficiary(
+//   welfareCaseId: string,
+//   beneficiaryId: string,
+// ): Promise<ApiResponse<null>> {
+//   const welfareCase = await dbClient.db.query.WelfareCases.findFirst({
+//     where: eq(schema.WelfareCases.id, welfareCaseId),
+//     columns: { id: true },
+//   });
 
-  if (!welfareCase) {
-    throw new ApiError("Welfare case not found", 404);
-  }
+//   if (!welfareCase) {
+//     throw new ApiError("Welfare case not found", 404);
+//   }
 
-  await welfareService.removeWelfareCaseBeneficiary(
-    welfareCaseId,
-    beneficiaryId,
-  );
+//   await welfareService.removeWelfareCaseBeneficiary(
+//     welfareCaseId,
+//     beneficiaryId,
+//   );
 
-  return {
-    success: true,
-    message: "Beneficiary removed successfully",
-    data: null,
-  };
-}
+//   return {
+//     success: true,
+//     message: "Beneficiary removed successfully",
+//     data: null,
+//   };
+// }
 
-export async function getWelfareCaseEvents(
-  welfareCaseId: string,
-  query: { page?: number; pageSize?: number } = {},
-): Promise<
-  ApiResponse<Paginated<import("@/features/api/v1/events/dtos").YPFEvent>>
-> {
-  const data = await welfareService.fetchWelfareCaseEvents(
-    welfareCaseId,
-    query,
-  );
+// export async function getWelfareCaseEvents(
+//   welfareCaseId: string,
+//   query: { page?: number; pageSize?: number } = {},
+// ): Promise<
+//   ApiResponse<Paginated<import("@/features/api/v1/events/dtos").YPFEvent>>
+// > {
+//   const data = await welfareService.fetchWelfareCaseEvents(
+//     welfareCaseId,
+//     query,
+//   );
 
-  return {
-    success: true,
-    message: "Welfare case events fetched successfully",
-    data,
-  };
-}
+//   return {
+//     success: true,
+//     message: "Welfare case events fetched successfully",
+//     data,
+//   };
+// }
