fd57ca7202656780434e0cf13563bc2abe1f40d3
Isaac <nisaacdz@gmail.com>
2025-12-29 15:45:49 +0000

    Update features

--- features/api/v1/dashboard/dtos.ts
+++ features/api/v1/dashboard/dtos.ts
@@ -1,12 +1,10 @@
 import { Paginated } from "@/shared/dtos";
 import { YPFProject } from "../projects/dtos";
-import { YPFWelfareCase } from "../welfare/dtos";
 import { YPFEvent } from "../events/dtos";
 import { ShopProduct } from "../shop/dtos";
 
 export type Activity = {
   projects: Paginated<YPFProject>;
-  welfareProjects: Paginated<YPFWelfareCase>;
   workshopEvents: Paginated<YPFEvent>;
   shopProducts: Paginated<ShopProduct>;
 };
